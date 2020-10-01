138df0600d91891c1e76423ccd6135253f0b9d87
kesihain <kesihainselvarajoo@gmail.com>
2020-10-02 00:54:03 +0800

    fixed email and warnings

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import {Link} from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
 
 function Navbar(){
     return (
